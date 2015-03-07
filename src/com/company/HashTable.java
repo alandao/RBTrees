5f64a901fa86f7a78db5a7fe9d3ad16bee01aaf9
Alan Dao <alan.n.dao@gmail.com>
2015-03-07 00:13:35 -0800

    added insert remove for hashtable functions

--- src/com/company/HashTable.java
+++ src/com/company/HashTable.java
@@ -3,23 +3,73 @@ package com.company;
 /**
  * Created by alandao on 3/6/15.
  */
-public class HashTable<K, V> {
-    private Object[] arr;
+public class HashTable {
+    private LinkedList[] arr;
+
+    class LinkedList {
+        Node head;
+        public LinkedList(Node node) {
+            head = node;
+        }
+    }
+    class Node{
+        Object key;
+        Object value;
+        Node next;
+        public Node(Object key, Object value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
 
     public HashTable(int size) {
-        arr = new Object[size];
+        arr = new LinkedList[size];
     }
 
-    public void Insert(K key, V value) {
-
+    public void Insert(Object key, Object value) {
+        int location = key.hashCode() % arr.length;
+        if (arr[location] == null) {
+            arr[location] = new LinkedList(new Node(key, value));
+        }
+        else {
+            //temp variable to not destroy our chained list
+            Node temp = arr[location].head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = new Node(key, value);
+        }
     }
 
-    public void Remove(K key, V Value) {
-
+    public void Remove(Object key) {
+        int location = key.hashCode() % arr.length;
+        if (arr[location] == null) {
+            //do nothing
+        } else if (arr[location].head.next == null) { //first node is key
+            arr[location] = null;
+        } else { //encountered a chain, deal accordingly
+            //temp variable to not destroy our chained list
+            Node temp = arr[location].head;
+            while (temp.next != key) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+        }
     }
-
+    /*
     public V Find(K Key) {
 
     }
+    */
+    private boolean containsKey(Object key) {
+        int location = key.hashCode() % arr.length;
+        if (arr[location] == null) {
+            return false;
+        }
+        else {
+            return true;
+        }
+    }
 
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,11 +9,12 @@ public class Main {
     public static void main(String[] args) throws FileNotFoundException {
 	// write your code here
         File text = new File("players_homeruns.csv");
-
+        HashTable<String, Integer> playersToScore = new HashTable<String, Integer>(10);
         Scanner scnr = new Scanner(text);
 
         while (scnr.hasNextLine()) {
             String line = scnr.nextLine();
+            playersToScore.Insert("Hi", 0);
 
         }
     }
