17f3f4bfaf3aecf66e8b0f93a886f3a14280ad05
Alan on Lenovo <alan.n.dao@gmail.com>
2015-04-09 09:17:08 -0700

    added helper functions

--- lab4.hs
+++ lab4.hs
@@ -4,3 +4,16 @@ data RBTree a = Empty | Node a Color (RBTree a) (RBTree a) deriving (Eq, Show)
 main :: IO()
 main = putStrLn "Undefined"
 
+--helper functions
+rbtLeftChild :: (Ord a) => RBTree a -> RBTree a
+rbtLeftChild (Node _ _ l _) = l
+
+rbtRightChild :: (Ord a) => RBTree a -> RBTree a
+rbtRightChild (Node _ _ _ r) = r
+
+rbtValue :: (Ord a) => RBTree a -> a
+rbtValue (Node v _ _ _) = v
+
+rbtColor :: (Ord a) => RBTree a -> Color
+rbtColor (Node _ c _ _) = c
+rbtColor Empty = Black
